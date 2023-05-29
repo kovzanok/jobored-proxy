85d7b2d96788a247767e1b78c0f7324229a41092
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-29 11:31:09 +0300

    feat: add store cookies

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ import express from "express";
 import cors from "cors";
 import proxy from "express-http-proxy";
 import requestIp from "request-ip";
+import { LRUCache } from "lru-cache";
+
 const app = express();
 const port = 3001;
 const apiUrl='https://api.superjob.ru/'
+const cookiePerIP = new LRUCache({
+  max: 1000,
+  // how long to live in ms
+  ttl: 1000 * 60 * 3, // 3 min
+  updateAgeOnGet: false,
+  updateAgeOnHas: false,
+});
+
 app.use(
   cors({
     origin: true,
@@ -17,6 +27,37 @@ app.use(
   "/",
   proxy(apiUrl, {
     https: true,
+    userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+      const key = userReq.clientIp;
+
+      if (headers["set-cookie"]) {
+        const newCookies = headers["set-cookie"].map((c) => {
+          const [key, value] = c.split(";")[0].split("=");
+          return { key, value };
+        });
+
+        const previousCookies = cookiePerIP.get(key) || [];
+        const currentCookies = previousCookies.concat(newCookies);
+
+        cookiePerIP.set(key, currentCookies);
+      }
+
+      return headers;
+    },
+    proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+      const key = srcReq.clientIp;
+
+      if (cookiePerIP.has(key)) {
+        const cookies = cookiePerIP
+          .get(key)
+          .map((c) => `${c.key}=${c.value}`)
+          .join(";");
+
+        proxyReqOpts.headers["cookie"] = cookies;
+      }
+
+      return proxyReqOpts;
+    },
   })
 );
 app.listen(port, () => {
