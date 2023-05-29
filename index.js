fc5e9de1e82e857be2caca038dc53a44f289baca
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-29 11:29:11 +0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 3001;
+app.use(
+  cors({
+    origin: true,
+  })
+);
 app.listen(port, () => {
   console.log("Server started");
 });
