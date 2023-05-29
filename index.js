d2565636b9c15c26a8421853a5b08698c2a83071
Alex Kovzan <alexanderkovzan@gmail.com>
2023-05-29 11:29:55 +0300

    feat: add proxy

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
+import proxy from "express-http-proxy";
 const app = express();
 const port = 3001;
+const apiUrl='https://api.superjob.ru/'
 app.use(
   cors({
     origin: true,
+  })
+);
+app.use(
+  "/",
+  proxy(apiUrl, {
+    https: true,
   })
 );
 app.listen(port, () => {
